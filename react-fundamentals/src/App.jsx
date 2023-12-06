84847839c4e9fea171c23fae808b9669f0acbddb
Andrei Campigotto <andreicampigotto@Andreis-MacBook-Pro.local>
2023-12-06 11:47:54 -0400

    Created cards and inital styles in the cards

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -5,23 +5,28 @@ import Parameter from "./components/basics/Parameter";
 import Fragment from "./components/basics/Fragment";
 import Aleatory from "./components/basics/Aleatory";
 import Card from "./components/layouts/Card";
+import "./App.css";
 
 export default (_) => (
-  <div id="App">
-    <Card title="First Component">
-      <First />
-    </Card>
+  <div className="App">
+    <h1>React Fundamentals</h1>
 
-    <Card title="Parameter">
-      <Parameter title="Title" subtitle="Subtitle" />
-    </Card>
+    <div className="Cards">
+      <Card title="#01 First Component">
+        <First />
+      </Card>
 
-    <Card title="Fragment">
-      <Fragment />
-    </Card>
+      <Card title="#02 Parameter">
+        <Parameter title="Title" subtitle="Subtitle" />
+      </Card>
 
-    <Card title="Random challenge">
-      <Aleatory min={0} max={99} />
-    </Card>
+      <Card title="#03 Fragment">
+        <Fragment />
+      </Card>
+
+      <Card title="#04 Random challenge">
+        <Aleatory min={0} max={99} />
+      </Card>
+    </div>
   </div>
 );
