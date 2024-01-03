e3d163926ee6472a306b7469feffb2a0e3dba00b
Andrei Campigotto <andreicampigotto@Andreis-MacBook-Pro.local>
2024-01-02 23:58:37 -0400

    last commit before challenge

--- react-fundamentals/src/components/counter/Buttons.jsx
+++ react-fundamentals/src/components/counter/Buttons.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default (props) => {
   return (
     <div>
-      <button onClick={props.setInc}>+</button>
-      <button onClick={props.setDec}>-</button>
+      <button onClick={props.setInc}> + </button>
+      <button onClick={props.setDec}> - </button>
       <button onClick={props.setIncPitch}> + pitch</button>
       <button onClick={props.setDecPitch}> - pitch</button>
     </div>
